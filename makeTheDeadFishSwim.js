3a68598cccb9ee772457b43fbfece3bfaa07df42
Сергей <55275237+Zheleznikov@users.noreply.github.com>
2020-01-29 15:19:11 +0300

    Update makeTheDeadFishSwim.js
    https://www.codewars.com/kata/51e0007c1f9378fa810002a9
    
    Write a simple parser that will parse and run Deadfish.
    Deadfish has 4 commands, each 1 character long:
    i increments the value (initially 0)
    d decrements the value
    s squares the value
    o outputs the value into the return array
    Invalid characters should be ignored.
    
    решение - https://www.youtube.com/watch?v=-xXBKf4_W_8

--- makeTheDeadFishSwim.js
+++ makeTheDeadFishSwim.js
@@ -1,3 +1,18 @@
+/* 
+https://www.codewars.com/kata/51e0007c1f9378fa810002a9
+
+Write a simple parser that will parse and run Deadfish.
+Deadfish has 4 commands, each 1 character long:
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+решение - https://www.youtube.com/watch?v=-xXBKf4_W_8
+*/
+
+
 function parse(data) {
   let fish = 0;
   let sea = [];
