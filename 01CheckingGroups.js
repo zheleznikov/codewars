4cd0d355aca33cb44371c4b81d7de70020e4df0c
Сергей <55275237+Zheleznikov@users.noreply.github.com>
2020-03-14 20:49:54 +0300

    Update 01CheckingGroups.js

--- 01CheckingGroups.js
+++ 01CheckingGroups.js
@@ -1,32 +1,38 @@
 // https://www.codewars.com/kata/54b80308488cb6cd31000161
 
     function groupCheck(s) {
-        let openCount = 0;
-        let closeCount = 0;
         let stack = [];
-
-        s.split('').forEach(bracket => {
+        for (let bracket of s) {
             let l = stack.length - 1;
-
             if (['(', '{', '['].includes(bracket)) {
-                stack.push(bracket);
-                openCount++; // неидеальное условие ниже
+                stack.push(bracket); //неидеальная конструкция
             } else if ((stack[l] === '[' && bracket === ']') || (stack[l] === '(' && bracket === ')') || (stack[l] === '{' && bracket === '}')) {
                 stack.pop();
-                closeCount++;
             } else {
-                closeCount++; // как обойтись без счетчиков
+                stack.push(bracket);
             }
-        })
-
-        
-        // неидеальный return
-        if (openCount !== closeCount) {
-            return false
         }
-        
         return stack.length === 0;
-
     }
+
+// другое решение, которое можно разобрать
+ function groupCheck(s) {
+   var brackets = {"(": ")", "{": "}", "[": "]"}
+   var stack = []
+   
+   for (var c of s) {
+     if (c in brackets) {
+       stack.push(c)
+     } else if (brackets[stack[stack.length - 1]] == c) {
+       stack.pop()
+     } else {
+       return false
+     }
+   }
+   return stack.length == 0
+ }
+
+
+
 
     console.log(groupCheck("[()]]"));
