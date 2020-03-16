04602700f777ef84949e90d177a6cf220fa11d47
Сергей <55275237+Zheleznikov@users.noreply.github.com>
2020-03-16 14:13:42 +0300

    Update 03Highest Rank Number in an Array.js

--- 03Highest Rank Number in an Array.js
+++ 03Highest Rank Number in an Array.js
@@ -1,21 +1,22 @@
 // https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004
 
-// 1 вариант
   function highestRank(arr) {
-    const obj = arr.reduce((prevValue, num) => {
-      if (!prevValue[num]) {
-        prevValue[num] = 1;
+    const quantity = arr.reduce((obj, n) =>{
+      if (!obj[n]) {
+        obj[n] = 1;
       } else {
-        prevValue[num] += 1;
+        obj[n] += 1;
       }
-      return prevValue
-    }, {});
-    
-    const rank = Math.max.apply(null, Object.values(obj));
-    const keys = Object.keys(obj);
-    const array = keys.filter(key => obj[key] === rank);
-    return Math.max.apply(null, array);
+      return obj
+    },{})
+  
+    const maxRepeat = Math.max.apply(null, Object.values(quantity));
+    const keys = Object.keys(quantity);
+    const oftRepeated = keys.filter(key => quantity[key] === maxRepeat);
+    return Math.max.apply(0, oftRepeated);
+
   }
+  console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]))
   
   
   // 
