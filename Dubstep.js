0eac895a20a405820dc7feadaa6a2de8fc2bed3d
Сергей <55275237+Zheleznikov@users.noreply.github.com>
2020-04-10 12:54:19 +0300

    Delete Dubstep.js

--- Dubstep.js
+++
@@ -1,8 +0,0 @@
-/* 
-https://www.codewars.com/kata/551dc350bf4e526099000ae5
-
-Нужно понять что за слова в песне, убрав из строки WUB
-
-*/
-
-songDecoder = song  => song.split('WUB').filter(elem => elem !== "").join(' ');
